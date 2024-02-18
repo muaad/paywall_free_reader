df4e9b2766ad4a2c125959c54b67950fb5d379ae
Muaad Abdirahman <muadh24@gmail.com>
2024-02-18 04:55:40 +0300

    Handle errors related to fetching the URL

--- lib/url_reader.rb
+++ lib/url_reader.rb
@@ -10,15 +10,19 @@ class UrlReader
 	def extract_main_content
 		return "<div class='alert alert-danger' role='alert'>Invalid URL</div>" unless @url && @url =~ URI::regexp
 		
-		page = @agent.get(@url)
+		begin
+			page = @agent.get(@url)
 	  
-		content = Readability::Document.new(
-			page.body,
-			tags: %w[div p img video iframe a h1 h2 h3 h4 h5 h6 figure ul ol li code pre blockquote], 
-			attributes: %w[src href], 
-			remove_empty_nodes: false
-		).content
+			content = Readability::Document.new(
+				page.body,
+				tags: %w[div p img video iframe a h1 h2 h3 h4 h5 h6 figure ul ol li code pre blockquote], 
+				attributes: %w[src href], 
+				remove_empty_nodes: false
+			).content
 
-		"<h2>#{page.title}</h2><hr><br>#{content}"
+			"<h2>#{page.title}</h2><hr><br>#{content}"
+		rescue Exception => e
+			"<div class='alert alert-danger' role='alert'>Ooops! An error has occurred fetching your URL.</div>"
+		end
 	end
 end
